51399f88c521b5e4e954e7cec08973b733e32a63
theoverse <theoverse@protonmail.com>
2022-02-28 12:08:54 -0600

    added put operation to update (replace) product info

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ uuidv4();
 let hostname = '127.0.0.1';
 let port = 3000;
 
-app.get('/', (req, res) => {
-    console.log(`Server is listening at ${hostname} on port ${port}.`);
-
-    res.send('Welcome banner');
-})
-
-app.listen(port, () => {
-    console.log(`Server is listening at http://${hostname}:${port}/`)
-})
-
 const products = [
     {
         id: '1',
@@ -36,6 +26,12 @@ const products = [
         price: '40'
     }
 ]
+
+app.get('/', (req, res) => {
+    console.log(`Server is listening at ${hostname} on port ${port}.`);
+
+    res.send('Welcome banner');
+})
 
 // show list of products
 app.get('/api/products', (req, res) => {
@@ -60,31 +56,64 @@ app.get('/api/products/:id', (req, res) => {
 app.use(express.json());
 app.post('/api/products', (req, res) => {
 
-    const schema = Joi.object({
-        name: Joi.string().min(3).max(20).required(),
-        price: Joi.number().required()
-    })
-
-    const { error } = schema.validate(req.body);
+    const { error } = validation(req.body)
 
     if (error) {
         return res.status(400).json({
             message: error.details[0].message
         })
-    }
+    };
 
     const product = {
         id: uuidv4(),
         name: req.body.name,
         price: req.body.price
-    }
-    products.push(product)
+    };
+    products.push(product);
 
-    return res.json(product)
+    return res.json(product);
 })
 
-
 // update specific producy data (using PUT method)
+app.put('/api/products/:id', (req, res) => {
+
+    const { error } = validation(req.body)
+
+    if (error) {
+        return res.status(400).json({
+            message: error.details[0].message
+        })
+    };
+
+    const index = products.findIndex(prod => prod.id === req.params.id);
+
+    if (index === -1) {
+        return res.status('404').json({
+            message: 'Product is not found with this id'
+        })
+    };
+
+    products[index].name = req.body.name;
+    products[index].price = req.body.price;
+
+    return res.json({
+        product: products[index]
+    })
+})
+
 // update specific product data (using patch method)
 // delete a specific product data
 // delete all products data
+
+function validation(body) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(20).required(),
+        price: Joi.number().required()
+    })
+
+    return schema.validate(body);
+}
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://${hostname}:${port}/`)
+})
