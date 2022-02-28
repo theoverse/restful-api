acb05516d6f6587440b72c508adb2740e708b8b5
theoverse <theoverse@protonmail.com>
2022-02-28 07:23:21 -0600

    added products array and get operatoin

--- index.js
+++ index.js
@@ -14,3 +14,33 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening at http://${hostname}:${port}/`)
 })
+
+const products = [
+    {
+        id: '1',
+        name: 'Apple',
+        price: '20'
+    },
+    {
+        id: '2',
+        name: 'Orange',
+        price: '30'
+    },
+    {
+        id: '3',
+        name: 'Pineapple',
+        price: '40'
+    }
+]
+
+// show list of products
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+// show specific products
+// insert a product data
+// update specific producy data (using PUT method)
+// update specific product data (using patch method)
+// delete a specific product data
+// delete all products data
