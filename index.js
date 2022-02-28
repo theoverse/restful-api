4cb516db5afa237e38a5bd5d3af860c954d18db0
theoverse <theoverse@protonmail.com>
2022-02-28 10:49:35 -0600

    installed joi and added validation to body input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 
 const { v4: uuidv4 } = require('uuid');
 uuidv4();
@@ -58,6 +59,19 @@ app.get('/api/products/:id', (req, res) => {
 // insert a product data
 app.use(express.json());
 app.post('/api/products', (req, res) => {
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(20).required(),
+        price: Joi.number().required()
+    })
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            message: error.details[0].message
+        })
+    }
 
     const product = {
         id: uuidv4(),
